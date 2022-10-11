0ad6f7456d975c88cc6e35b000fbfb207bdf65ee
Eric Bowser <ericbo@engineer-it-all.com>
2022-10-11 03:55:03 -0600

    Add SQL scripts for calc operation

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express')
 // const router = require('express-router')
 const app = express();
+var sql = require('mssql')
+const {re} = require("mathjs");
 
 // app.METHOD(PATH, HANDLER)
 console.log('passed port to use', process.env.port | 3000);
 const port = process.env.port | 3000;
 
-app.get("/docs/openapi_3.yaml", (req, res) => {
+app.get("/", (req , res) => {
     console.log("default route");
+    console.log("Fetching users");
+    
+    const config = {
+        user: 'test',
+        password: 'test',
+        server: '(localdb)\MSSQLLocalDB',
+        database: 'Login'
+    }
+    
+    sql.connect(config, () => {
+        console.log('connected to db');
+    });
+    
+    
+    const result = sql.query("SELECT * FROM [dbo].[User]")
+        .then(res => {
+            console.log(res)
+        });
+    
+    console.log("fetched result: ", result);
+    sql.close();
+    
+    return result;
 });
 
 app.post("/add", (req, res) => {
