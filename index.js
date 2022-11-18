6a36da3ce53d0ca457ae5ce501bccdcbdc8a0336
Eric Bowser <ericbo@engineer-it-all.com>
2022-11-18 03:22:41 -0700

    env package

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const server = require('./server');
 const dotenv = require('dotenv');
 dotenv.config();
 
-console.log('passed port to use', 5000);
 const port = process.env.PORT || 5000;
+console.log('passed port to use', port);
 const app = express();
 app.use(server);
 
